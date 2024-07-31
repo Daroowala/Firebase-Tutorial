2730c4f68fa4bf472435aeb9994cbc22e60fe373
my name <blueshaperd3@gmail.com>
2024-08-01 03:29:24 +0500

    create account ok

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-analytics.js";
-  import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+  import { getAuth, onAuthStateChanged, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
 
   const firebaseConfig = {
     apiKey: "AIzaSyBswAy79OlMNrdSF3p9WA4a13AVTWou568",
@@ -17,5 +16,34 @@
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
   const auth = getAuth(app);
-  console.log("app=>", app);
-console.log("auth=>", auth);
+  const signup_email = document.getElementById("singup_email");
+  const signup_password = document.getElementById("singup_password");
+  const signup_btn = document.getElementById("singup_btn");
+
+  signup_btn.addEventListener("click", createUserAccount)
+
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        console.log("User is logged in")
+    const uid = user.uid;
+    } else {
+        console.log("No user is signed in");
+    }
+  });
+
+  function createUserAccount(){
+    console.log("email=>",signup_email.value);
+    console.log("password=>",signup_password.value);
+    createUserWithEmailAndPassword(auth, signup_email.value, signup_password.value)
+  .then((userCredential) => {
+    
+    const user = userCredential.user;
+    console.log("User=>", user);
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    alert(errorMessage)
+  });
+  }

9bcbf72ea9224a9846d55e48dc9b355544933f12
my name <blueshaperd3@gmail.com>
2024-08-01 03:56:12 +0500

    signIn ok

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-analytics.js";
-  import { getAuth, onAuthStateChanged, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+  import { getAuth, 
+    onAuthStateChanged, 
+    createUserWithEmailAndPassword, 
+    signInWithEmailAndPassword } 
+    from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
 
   const firebaseConfig = {
     apiKey: "AIzaSyBswAy79OlMNrdSF3p9WA4a13AVTWou568",
@@ -16,11 +20,16 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/fireba
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
   const auth = getAuth(app);
-  const signup_email = document.getElementById("singup_email");
-  const signup_password = document.getElementById("singup_password");
-  const signup_btn = document.getElementById("singup_btn");
+  const signup_email = document.getElementById("signup_email");
+  const signup_password = document.getElementById("signup_password");
+  const signup_btn = document.getElementById("signup_btn");
 
-  signup_btn.addEventListener("click", createUserAccount)
+  const signin_email = document.getElementById("signin_email");
+  const signin_password = document.getElementById("signin_password");
+  const signin_btn = document.getElementById("signin_btn");
+
+  signup_btn.addEventListener("click", createUserAccount);
+  signin_btn.addEventListener("click", singIn);
 
 
 onAuthStateChanged(auth, (user) => {
@@ -46,4 +55,20 @@ onAuthStateChanged(auth, (user) => {
     const errorMessage = error.message;
     alert(errorMessage)
   });
+  }
+  function singIn(){
+    signInWithEmailAndPassword(auth, signin_email.value, signin_password.value)
+  .then((userCredential) => {
+     
+    const user = userCredential.user;
+   console.log("User")
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    alert(errorMessage)
+  });
+
+    // console.log("email=>", signin_email.value);
+    // console.log("password=>", signin_password.value);
   }
